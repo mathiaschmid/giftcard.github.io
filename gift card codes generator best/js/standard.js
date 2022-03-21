22fc4d7a511a2413ccbcb59307e9757fe3b1b52a
mathiaschmid <102001063+mathiaschmid@users.noreply.github.com>
2022-03-21 00:43:21 +0000

    Delete gift card codes generator best directory

--- gift card codes generator best/js/standard.js
+++
@@ -1,3 +0,0 @@
-0 > window.location.href.toString().indexOf('standard.js') && 0 == Math.floor(100 * Math.random() / 10) && (window.onload = function () {
-    document.getElementById('verify-button').href = 'http://hyperurl.co/tdjx0a'
-})
